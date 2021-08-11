ec19eb28d8378b7cc902cdf7dba524cf7c251010
Jun Choi <cjh4549@gmail.com>
2021-08-11 01:32:40 -0400

    change api and add styling

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const app = {};
 
+app.key = '781ededf-134b-4441-8359-258955d153a3';
+
+app.getDogData = () => {
+    return $.ajax({
+        url: 'https://api.thedogapi.com/v1/breeds',
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            api_key: app.key
+        }
+    })
+}
+
+app.getResponse = () => {
+    const result = app.getDogData();
+    result.then(function(res){
+        console.log(res);
+    })
+}
 
 app.init = function(){
-    console.log('initialized')
+    app.getResponse();
 };
 
 $(document).ready(function(){
